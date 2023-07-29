582e6fa6e8eca87dd0764dbdb5c2808bc34b4696
2493787138 <2493787138@qq.com>
2023-07-29 20:53:17 +0800

    7.29 cyn

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,44 @@
 <template>
-  <div class="container">
-    <el-main>
-      <div class="title-img">
-        <img src="../assets/picture/titleimg.png">
-      </div>
-      <div class="left-side-img">
-        <img src="../assets/picture/loginbg.png">
-      </div>
-      <el-form
-          label-width="70px"
-          ref="form"
-          :model="form"
-          :rules="rules"
-          v-show="showLoginForm"
-          class="loginform"
-          inline
-      >
-        <h3 class="login_title">系统登录</h3>
-        <el-form-item prop="username" label="用户:">
-          <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item prop="password" label="密码:" inline>
-          <el-input
-              type="password"
-              v-model="form.password"
-              placeholder="请输入密码"
-          ></el-input>
-        </el-form-item>
-        <div></div>
-        <el-form-item>
-          <el-button type="primary" @click="submit" class="button">登录</el-button>
-          <el-button type="primary" @click="goregister" class="button">注册</el-button>
-          <br/><br/>
-          <a>忘记密码？</a>
-        </el-form-item>
-      </el-form>
-    </el-main>
-  </div>
+  <el-container class="container">
+    <el-header></el-header>
+    <el-row class="row">
+      <el-col class="left" :span="15">
+        
+          <img class="title-img" src="../assets/picture/titleimg.png" >
+          <img class="left-side-img animate__animated animate__fadeInLeft" src="../assets/picture/loginbg.png">
+        
+      </el-col>
+      <el-col :span="9" class="right">
+        <el-form label-width="70px" ref="form" :model="form" :rules="rules" v-show="showLoginForm" class="loginform"
+          inline>
+          <h3 class="login_title">系统登录</h3>
+          <el-form-item prop="username" label="用户:">
+            <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="password" label="密码:" inline>
+            <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <div></div>
+          <el-form-item>
+            <el-button type="primary" @click="submit" class="button">登录</el-button>
+            <el-button type="primary" @click="goregister" class="button">注册</el-button>
+            <br /><br />
+            <a>忘记密码？</a>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+
+
+
+
+  </el-container>
 </template>
 
 <script>
 import Cookie from 'js-cookie';
-import {login} from '../api'
+import { login } from '../api';
+import 'animate.css';
 
 export default {
   data() {
@@ -53,10 +50,10 @@ export default {
       rules: {
         //trigger何时校验，blur失去焦点时，change值改变时
         username: [
-          {required: true, message: '请输入用户名', trigger: 'blur'}
+          { required: true, message: '请输入用户名', trigger: 'blur' }
         ],
         password: [
-          {required: true, message: '请输入密码', trigger: 'blur'}
+          { required: true, message: '请输入密码', trigger: 'blur' }
         ],
       },
       showLoginForm: true
@@ -72,7 +69,7 @@ export default {
       this.$refs.form.validate((valid) => {
         if (valid) {
 
-          login(this.form).then(({data}) => {
+          login(this.form).then(({ data }) => {
 
             if (data[0]) {
               console.log(data[0])
@@ -100,49 +97,69 @@ export default {
 </script>
 <style lang="less" scoped>
 .container {
+
   background-color: #dfeffa;
-  position: absolute;
-  top: 0;
-  left: 0;
   height: 100%;
   width: 100%;
 
-  .title-img {
-    position: absolute;
-    top: 2%;
-    left:17%;
-    width: 450px;
-    height: auto;
+  .el-row {
+    height: 82%;
+    width: 100%;
+    .left {
+      padding-left: 7%;
+      height: 100%;
+      
+      .title-img {
+        object-fit: cover;
+        padding-left: 10%;
+        padding-right: 10%;
+        width: 80%;
+        height: 20%;
       }
 
-  .left-side-img {
-    position: absolute;
-    top: 13%;
-    left:2%;
-    width: 50px;
-    height: 35%;
+      .left-side-img {
+        object-fit: cover;
+        width: 100%;
+        height: 80%;
+
+      }
+    }
+
+    .right {
+      padding-right: 7%;
+      padding-top: 5%;
+      height: 100%;
+
+      .loginform {
+        width: 75%;
+        height: 80%;
+        border: solid 1px;
+        border-color: #eaeaea;
+        padding-left: 35px;
+        padding-right: 40px;
+        padding-top: 35px;
+        padding-bottom: 15px;
+        border-radius: 15px;
+        box-shadow: 0 0 25px #cac6c6;
+        
+        text-align: center;
+        align-items: center;
+        background-color: #fff;
+
+        /* 将背景颜色修改为 #c28e8e */
+      }
+    }
 
   }
 
-  .loginform {
-    position: absolute;
-    top: 24%;
-    left: 67%;
-    width: 30%;
-    height: 60%;
-    border: solid 1px;
-    border-color: #eaeaea;
-    padding-left: 35px;
-    padding-right: 40px;
-    padding-top: 35px;
-    padding-bottom: 15px;
-    border-radius: 15px;
-    box-shadow: 0 0 25px #cac6c6;
-    box-sizing: border-box;
-    text-align: center;
-    align-items: center;
-    background-color: #fff; /* 将背景颜色修改为 #c28e8e */
+
+  .el-header {
+    height: 18%;
+    width: 100%;
   }
+
+
+
 
   .login_title {
 
@@ -162,5 +179,4 @@ export default {
     margin-left: 40px;
   }
 }
-
 </style>
